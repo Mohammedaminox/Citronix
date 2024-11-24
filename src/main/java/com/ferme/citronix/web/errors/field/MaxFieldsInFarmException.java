4713ccaf12e2261be0ef858e2e5e0c9dd1b0114b
amineBd <aminebenmade1999@gmail.com>
2024-11-24 17:23:27 +0100

    add MaxFieldsInFarmException

---
+++ src/main/java/com/ferme/citronix/web/errors/field/MaxFieldsInFarmException.java
@@ -0,0 +1,7 @@
+package com.ferme.citronix.web.errors.field;
+
+public class MaxFieldsInFarmException extends RuntimeException {
+    public MaxFieldsInFarmException(String message) {
+        super(message);
+    }
+}
