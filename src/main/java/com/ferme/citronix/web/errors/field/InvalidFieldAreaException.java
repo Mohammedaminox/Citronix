c21cadc0f7eed76f423ddd3845f002c749b85fdb
amineBd <aminebenmade1999@gmail.com>
2024-11-24 17:22:26 +0100

    add InvalidFieldAreaException

---
+++ src/main/java/com/ferme/citronix/web/errors/field/InvalidFieldAreaException.java
@@ -0,0 +1,7 @@
+package com.ferme.citronix.web.errors.field;
+
+public class InvalidFieldAreaException extends RuntimeException {
+    public InvalidFieldAreaException(String message) {
+        super(message);
+    }
+}
