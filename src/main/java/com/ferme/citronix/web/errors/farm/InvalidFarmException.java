0316f618192a0278a620a44f5eeb4651093a3e09
amineBd <aminebenmade1999@gmail.com>
2024-11-24 17:22:06 +0100

    add InvalidFarmException

---
+++ src/main/java/com/ferme/citronix/web/errors/farm/InvalidFarmException.java
@@ -0,0 +1,8 @@
+package com.ferme.citronix.web.errors.farm;
+
+public class InvalidFarmException extends RuntimeException {
+
+    public InvalidFarmException(String message) {
+        super(message);
+    }
+}
